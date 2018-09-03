f4370a12b8dff7ebc80f1b829c9ddbbf3a736ff3
Saif-M-Dhrubo <saif.dhrubo@gmail.com>
2018-09-03 14:41:38 +0600

    Twitter Data Retrieval

--- cricHub/app.py
+++ cricHub/app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    #return render_template('index.html', title='Home', matches=get_matches()) #use this if u have api key
     return render_template('newsTwitter.html')
 
 
