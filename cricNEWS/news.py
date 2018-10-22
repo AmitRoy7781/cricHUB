e77d5332c5cf186deca94491cc866a7dfeb17e5c
AmitRoy7 <aroy7298@gmail.com>
2018-10-22 16:58:53 +0600

    News Details and More news part implemented

--- cricNEWS/news.py
+++ cricNEWS/news.py
@@ -65,13 +65,53 @@ def show_news(page_no=None):
     #     print()
     #     print("--------------------------------------------------------")
 
-    print(page_no)
+    # print(page_no)
     return render_template("news/news.html", news_data=data,page_no= page_no)
 
 
 
 @app.route("/news/more_news",methods=['POST', 'GET'])
 def more_news():
-    print(request.form["more_news"])
+    if 'username' not in session.keys():
+        return redirect('/auth/signin')
+    # print(request.form["more_news"])
     page_no = int(request.form["more_news"])
     return show_news(str(page_no+1))
+
+
+@app.route("/news/detail_news",methods=['POST', 'GET'])
+def detail_news():
+    if 'username' not in session.keys():
+        return redirect('/auth/signin')
+
+    url = request.form["detail_news"]
+    url = "https://" + url
+
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'lxml')
+    news_topic = soup.find_all('span', {'class': 'cb-text-gray'})[0].text
+    news_headline = soup.find_all('h1', {'itemprop': 'headline'})[0].text
+    news_author = soup.find_all('span', {'itemprop': 'author'})[0].text
+    news_time = soup.find_all('time', {'itemprop': 'dateModified'})[0].text
+    news_img = soup.find_all('img', {'itemprop': 'contentUrl'})[0]["src"]
+    news_img_caption = soup.find_all('div', {'class': 'cb-img-cptn'})[0].text
+
+    all_paras = soup.find_all('p')
+    news_para = []
+    for temp in all_paras:
+        if temp.text != "" and temp.text != "{{suggest.tag}}" and temp.text != "Search for “”":
+            news_para.append(temp.text)
+
+    news_fb_href = soup.find_all('a', {'class': 'cb-social-ancr-fb'})[0]["href"]
+    news_twt_href = soup.find_all('a', {'class': 'cb-social-ancr-twt'})[0]["href"]
+
+    return render_template('/news/detail_news.html',
+                           news_topic=news_topic,
+                           news_headline=news_headline,
+                           news_author=news_author,
+                           news_time=news_time,
+                           news_img=news_img,
+                           news_img_caption=news_img_caption,
+                           news_para = news_para,
+                           news_fb_href=news_fb_href,
+                           news_twt_href=news_twt_href)
