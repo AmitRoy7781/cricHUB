2e5fb9bb36a4bf4eceab3719c01e21235146293a
Tauhid13 <tauhidtanjim13@gmail.com>
2018-10-24 00:05:32 +0600

    modified

--- cricCHAT/server.py
+++ cricCHAT/server.py
@@ -9,22 +9,4 @@ app.debug = True
 app.config['SECRET_KEY'] = 'nuttertools'
 socketio = SocketIO(app)
 
-@app.route('/')
-def chat():
-  return render_template('chat.html')
 
-@app.route('/login')
-def login():
-  return render_template('login.html')
-
-@socketio.on('message', namespace='/chat')
-def chat_message(message):
-  print("message = ", message)
-  emit('message', {'data': message['data']}, broadcast=True)
-
-@socketio.on('connect', namespace='/chat')
-def test_connect():
-  emit('my response', {'data': 'Connected', 'count': 0})
-
-if __name__ == '__main__':
-  socketio.run(app)
