785aee2fef38420c0c82abfa1d868f35a0f657fb
thtonmoy <thTonmoy@users.noreply.github.com>
2018-10-25 17:36:17 +0600

    update requirements, removed all socket-io code, frontend fixes

--- cricHUB/app.py
+++ cricHUB/app.py
@@ -3,9 +3,6 @@ from cffi.setuptools_ext import execfile
 from flask import Flask, redirect, session, render_template, make_response, request
 
 # start
-from gevent import monkey
-monkey.patch_all()
-from flask_socketio import SocketIO, emit, join_room
 from cricMongoDB.database import db
 import pymongo, json
 # end
@@ -28,17 +25,14 @@ app.secret_key = 'TishuPaperIsNoMore'
 
 # start
 app.debug = True
-socketio = SocketIO(app)
 # end
 
 
 # authentication blueprint
 app.register_blueprint(auth)
 
-
 # live score blueprint
 app.register_blueprint(livescore)
-
 
 # prediction blueprint
 app.register_blueprint(prediction)
@@ -70,7 +64,7 @@ def home():
     #     return redirect('/auth/signin')
     from cricSchedule import schedule_adapter
     data = schedule_adapter.Adapter()
-    return render_template('index.html', matches = data.get_match_data())
+    return render_template('index.html', matches=data.get_match_data())
 
 
 @app.template_filter('formatdatetime')
@@ -84,7 +78,6 @@ def format_datetime(value, format="%a %d %B %I:%M %p"):
     return local_dt.strftime(format)
 
 
-
 # @app.route('/chat-box')
 # def chat():
 #    execfile('server.py')
@@ -93,64 +86,38 @@ def format_datetime(value, format="%a %d %B %I:%M %p"):
 # start
 @app.route('/chat-box/')
 def chat():
-
     if 'username' not in session.keys():
         return redirect('/auth/signin')
 
-    data = [];
-    data = db.chat.find();
+    data = []
+    data = db.chat.find()
 
-    message = [];
+    message = []
     for item in data:
-        tmp = [];
-        tmp.append(item["author"]);
-        tmp.append(item["message"]);
-        message.append(tmp);
-
+        tmp = []
+        tmp.append(item["author"])
+        tmp.append(item["message"])
+        message.append(tmp)
 
     if request.cookies.get('realtime-chat-nickname') is None:
         res = make_response(render_template("chat.html", list=message))
         res.set_cookie('realtime-chat-nickname', session['username'])
         print(res)
-        return  res
+        return res
 
     else:
 
-    #data = [];
-    #data = db.chat.find();
+        # data = [];
+        # data = db.chat.find();
 
-    #message = [];
-    #for item in data:
-    #    tmp = [];
-    #    tmp.append(item["author"]);
-    #    tmp.append(item["message"]);
-    #    message.append(tmp);
+        # message = [];
+        # for item in data:
+        #    tmp = [];
+        #    tmp.append(item["author"]);
+        #    tmp.append(item["message"]);
+        #    message.append(tmp);
 
         return render_template("chat.html", list=message)
-
-@socketio.on('message', namespace='/chat')
-def chat_message(message):
-
-    author = session["username"]
-
-    print("message = ", message)
-    print(author + " " + message["data"]["message"])
-
-    if message["data"]["message"] != "":
-
-        # insert into database
-        db.chat.insert({"author": author, "message": message["data"]["message"]});
-
-        message["data"]["author"] = author
-        emit('message', {'data': message['data']}, broadcast=True)
-
-
-@socketio.on('connect', namespace='/chat')
-def test_connect():
-
-    emit('my response', {'data': 'Connected', 'count': 0})
-
-# end
 
 
 if __name__ == '__main__':
