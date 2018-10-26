4f46e5cb139328caba0db2c072efa7df5000f5b9
Tauhid13 <tauhidtanjim13@gmail.com>
2018-10-26 23:11:52 +0600

    Writing in Blog Added

--- cricBLOG/blog.py
+++ cricBLOG/blog.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, session, Blueprint, json
 from bs4 import BeautifulSoup
 import requests
+
+import datetime
 from cricMongoDB.database import db
 
 app = Blueprint('blog', __name__)
@@ -17,11 +19,58 @@ def show_blog():
     message = []
     for item in data:
         tmp = []
+
         tmp.append(item["title"])
+
         tmp.append(item["author"])
+
         tmp.append(item["date"])
         tmp.append(item["content"])
         message.append(tmp)
         print(tmp[0]+" "+tmp[1]+" "+tmp[2]+" "+tmp[3])
 
+
     return render_template("/blog/show_blog.html", blog_data=message)
+
+
+
+
+@app.route('/blog/add/', methods=['GET', 'POST'])
+
+#To post something in
+#in this feature blog
+def add():
+
+    if request.method == 'POST':
+
+        title = request.form['title']
+
+        content = request.form['content']
+        if title=="" or content =="":
+            return redirect('/blog/add/')
+        else:
+            blog_message = {}
+
+            blog_message["author"] =session['username']
+
+            blog_message["title"] = title
+
+            blog_message["content"] = content
+
+            now = datetime.datetime.now()
+
+            now=now.replace(second=0, microsecond=0)
+
+            blog_message["date"]=str(now)
+
+            posts = db.blog
+
+            posts.insert_one(blog_message)
+
+            print(title+content+str(now))
+
+            return show_blog()
+
+    else:
+
+        return render_template("/blog/add.html")
