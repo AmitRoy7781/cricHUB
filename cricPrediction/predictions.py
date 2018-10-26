55ee81c108553a6605b2f756dea249f038adfbc8
thtonmoy <thTonmoy@users.noreply.github.com>
2018-10-26 13:50:32 +0600

    schedule on separate page, mandatory auth for prediction, handling same team selection

--- cricPrediction/predictions.py
+++ cricPrediction/predictions.py
@@ -1,10 +1,12 @@
-from flask import render_template, request, redirect, session, Blueprint
+from flask import render_template, request, redirect, session, Blueprint, flash
 from cricPrediction.predictor import ServePrediction
 
 app = Blueprint('predictions', __name__)
 
 @app.route('/predictions/')
 def predictions():
+    if 'username' not in session.keys():
+        return redirect('/auth/signin')
     return render_template("prediction/prediction.html")
 
 @app.route('/pred-deliver/', methods=['GET', 'POST'])
@@ -15,7 +17,8 @@ def get_prediction():
         team2 = request.form['team2']
         venue = request.form['venue']
     if team1 == team2:
-        return "Not found"
+        flash('Please select different teams')
+        return render_template("prediction/prediction.html")
     predictor = ServePrediction(app.root_path)
     pred = predictor.predict(team1, team2, venue)
     formatted_pred = predictor.get_formatted_prediction(team1, team2, pred)
@@ -24,4 +27,4 @@ def get_prediction():
     values1 = pred[0, :].flatten().tolist()
     values2 = pred[1, :].flatten().tolist()
     return render_template('prediction/pred-deliver.html',
-                           pred = formatted_pred,values1=values1, values2=values2, labels=labels, legend=legend)
+                               pred=formatted_pred, values1=values1, values2=values2, labels=labels, legend=legend)
