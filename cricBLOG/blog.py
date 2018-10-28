88c2ddf4caf63884b2bf28d26656c71ed9ff8ca3
RHT-20 <rakibulhasan8020@gmail.com>
2018-10-28 21:49:22 +0600

    Blog updated

--- cricBLOG/blog.py
+++ cricBLOG/blog.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, session, Blueprint, json
 from bs4 import BeautifulSoup
 import requests
-import text_to_image
 
 import datetime
 from cricMongoDB.database import db
@@ -18,57 +17,41 @@ def show_blog():
     data = db.blog.find()
 
     message = []
+
     for item in data:
         tmp = []
-
         tmp.append(item["title"])
-
         tmp.append(item["author"])
-
         tmp.append(item["date"])
         tmp.append(item["content"])
         message.append(tmp)
+
         print(tmp[0]+" "+tmp[1]+" "+tmp[2]+" "+tmp[3])
 
+    message.sort(key=lambda message:message[2])
+    message.reverse()
 
     return render_template("/blog/show_blog.html", blog_data=message)
 
 
-
-
+# To post something in feature blog
 @app.route('/blog/add/', methods=['GET', 'POST'])
-
-#To post something in
-#in this feature blog
 def add():
 
-    if 'username' not in session.keys():
-        return redirect('/auth/signin')
-
     if request.method == 'POST':
-
         title = request.form['title']
-
         content = request.form['imagecontent']
 
-
-        if title=="" or content =="":
+        if title == "" or content == "":
             return redirect('/blog/add/')
         else:
             blog_message = {}
-
-            blog_message["author"] =session['username']
-
+            blog_message["author"] = session['username']
             blog_message["title"] = title
-
             blog_message["content"] = content
-
             now = datetime.datetime.now()
-
-            now=now.replace(second=0, microsecond=0)
-
-            blog_message["date"]=str(now)
-
+            now = now.replace(second=0, microsecond=0)
+            blog_message["date"] = str(now)
             posts = db.blog
 
             posts.insert_one(blog_message)
@@ -78,5 +61,4 @@ def add():
             return redirect('/blog/')
 
     else:
-
-        return render_template("/blog/show_blog.html",blog_data=None)
+        return render_template("/blog/add.html")
