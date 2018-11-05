2a6534d260ae153fdcf4aaf56b87fd38ba2a0bda
AmitRoy7 <aroy7298@gmail.com>
2018-11-06 02:14:01 +0600

    Observer pattern in cricCHAT

--- cricCHAT/cricCHAT.py
+++ cricCHAT/cricCHAT.py
@@ -1,4 +1,5 @@
 import pusher
+import abc
 from cricAuth.auth import signin
 from flask import Flask,render_template,jsonify,request,Blueprint,session,redirect
 from cricMongoDB.database import db
@@ -14,42 +15,104 @@ pusher_client = pusher.Pusher(
 )
 
 
-@app.route('/chat-room/')
-def index():
 
-    if 'username' not in session.keys():
-        return signin(None, "/chat-room/")
-    messages = db.chat.find()
-    return render_template('chat/chat.html', messages=messages)
+class Subject:
+    """
+    Know its observers. Any number of Observer objects may observe a
+    subject.
+    Send a notification to its observers when its state changes.
+    """
+
+    def __init__(self):
+        self._observers = set()
+        self._subject_state = None
+
+    def attach(self, observer):
+        observer._subject = self
+        self._observers.add(observer)
+        self.register()
+
+    def detach(self, observer):
+        observer._subject = None
+        self._observers.discard(observer)
+
+    def _notify(self):
+        for observer in self._observers:
+            observer.update(self._subject_state)
+
+    @property
+    def subject_state(self):
+        return self._subject_state
+
+    @subject_state.setter
+    def subject_state(self, arg):
+        self._subject_state = arg
+        self._notify()
+
+    @app.route('/chat-room/')
+    def register(self=None):
+
+        if 'username' not in session.keys():
+            return signin(None, "/chat-room/")
+        messages = db.chat.find()
+        return render_template('chat/chat.html', messages=messages)
 
 
-@app.route('/message', methods=['POST'])
-def message():
+class Observer(metaclass=abc.ABCMeta):
+    """
+    Define an updating interface for objects that should be notified of
+    changes in a subject.
+    """
+
+    def __init__(self):
+        self._subject = None
+        self._observer_state = None
+
+    @abc.abstractmethod
+    def update(self, arg):
+        pass
 
 
-    if 'username' not in session.keys():
-        return signin(None, "/message/")
 
-    print(request.form.to_dict())
-    try:
+class ConcreteObserver(Observer):
+    """
+    Implement the Observer updating interface to keep its state
+    consistent with the subject's.
+    Store state that should stay consistent with the subject's.
+    """
 
-        username = request.form.get('author')
-        message = request.form.get('message')
+    def update(self, arg):
+        self._observer_state = arg
+        self.message()
 
-        #print(request.form.to_dict())
 
-        new_message ={}
-        new_message["author"] = username
-        new_message["message"] = message
-        posts = db.chat
-        posts.insert_one(new_message)
+    @app.route('/message', methods=['POST'])
+    def message(self=None):
 
-        pusher_client.trigger('my-channel', 'new-message', {'author': username, 'message': message})
 
-        return render_template('chat/chat.html')
+        if 'username' not in session.keys():
+            return signin(None, "/message/")
 
-    except:
+        # print(request.form.to_dict())
+        try:
 
-        return jsonify({'result': 'failure'})
+            username = request.form.get('author')
+            message = request.form.get('message')
+
+            #print(request.form.to_dict())
+
+            new_message ={}
+            new_message["author"] = username
+            new_message["message"] = message
+            posts = db.chat
+            posts.insert_one(new_message)
+
+            pusher_client.trigger('my-channel', 'new-message', {'author': username, 'message': message})
+
+            return render_template('chat/chat.html')
+
+        except:
+
+            return jsonify({'result': 'failure'})
 
 
