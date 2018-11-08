31a535d9d505be950888a92d0e2da2f9d07fc408
Tauhid13 <tauhidtanjim13@gmail.com>
2018-11-08 18:55:11 +0600

    Patterning

--- cricBLOG/blog.py
+++ cricBLOG/blog.py
@@ -2,40 +2,52 @@ from flask import render_template, request, redirect, session, Blueprint, json
 from cricAuth.auth import signin
 from bs4 import BeautifulSoup
 import requests
-
+import copy
 import datetime
 from cricMongoDB.database import db
 
 app = Blueprint('blog', __name__)
 
 @app.route('/blog/')
-def show_blog():
 
-    if 'username' not in session.keys():
-        return signin(None,"/blog/")
-
-    data = []
-    data = db.blog.find()
-
-    message = []
-
-    for item in data:
-        tmp = []
-        tmp.append(item["title"])
-        tmp.append(item["author"])
-        tmp.append(item["date"])
-        tmp.append(item["content"])
-        message.append(tmp)
-
-        # print(tmp[0]+" "+tmp[1]+" "+tmp[2]+" "+tmp[3])
-
-    message.sort(key=lambda message:message[2])
-    message.reverse()
-
-    return render_template("/blog/show_blog.html", blog_data=message)
+def showBlog():
+    prototype = Prototype()
+    prototype_copy = copy.deepcopy(prototype)
+    return prototype_copy.show_blog()
 
 
-# To post something in feature blog
+class Prototype:
+    """
+    Example class to be copied.
+    """
+
+
+    def show_blog(self):
+
+        if 'username' not in session.keys():
+            return signin(None, "/blog/")
+
+        data = []
+        data = db.blog.find()
+
+        message = []
+
+        for item in data:
+            tmp = []
+            tmp.append(item["title"])
+            tmp.append(item["author"])
+            tmp.append(item["date"])
+            tmp.append(item["content"])
+            message.append(tmp)
+
+            # print(tmp[0]+" "+tmp[1]+" "+tmp[2]+" "+tmp[3])
+
+        message.sort(key=lambda message: message[2])
+        message.reverse()
+
+        return render_template("/blog/show_blog.html", blog_data=message)
+
+
 @app.route('/blog/add/', methods=['GET', 'POST'])
 def add():
 
@@ -66,3 +78,33 @@ def add():
 
     else:
         return render_template("/blog/add.html")
+
+
+# @app.route('/blog/')
+# def show_blog():
+#
+#     if 'username' not in session.keys():
+#         return signin(None,"/blog/")
+#
+#     data = []
+#     data = db.blog.find()
+#
+#     message = []
+#
+#     for item in data:
+#         tmp = []
+#         tmp.append(item["title"])
+#         tmp.append(item["author"])
+#         tmp.append(item["date"])
+#         tmp.append(item["content"])
+#         message.append(tmp)
+#
+#         # print(tmp[0]+" "+tmp[1]+" "+tmp[2]+" "+tmp[3])
+#
+#     message.sort(key=lambda message:message[2])
+#     message.reverse()
+#
+#     return render_template("/blog/show_blog.html", blog_data=message)
+#
+
+# To post something in feature blog
